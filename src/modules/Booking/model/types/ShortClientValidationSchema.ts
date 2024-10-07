d426ecb68ce0a77f08a94f7abb9980594b7ba981
BastianHolmes <sebastian.holmes2001@gmail.com>
2024-10-07 18:56:13 +0300

    feat:short-client-creation

--- src/modules/Booking/model/types/ShortClientValidationSchema.ts
+++ src/modules/Booking/model/types/ShortClientValidationSchema.ts
@@ -2,10 +2,14 @@ import { OwnerDto } from "@/generated/owners"
 import * as yup from "yup"
 
 export const ShortClientSchema = yup.object<OwnerDto>().shape({
-  firstName: yup.string(),
-  lastname: yup.string(),
+  firstName: yup
+    .string()
+    .length(2, "Поле должно содержать не менее 2 символов"),
+  lastname: yup.string().length(2, "Поле должно содержать не менее 2 символов"),
   mainPhone: yup.string(),
-  middleName: yup.string(),
+  middleName: yup
+    .string()
+    .length(2, "Поле должно содержать не менее 2 символов"),
   optionalPhone: yup.string(),
   rating: yup.number(),
-  })
+})
