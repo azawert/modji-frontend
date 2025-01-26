75b98526e1e8a921de3490c11911801a845349c1
azawert <53710085+azawert@users.noreply.github.com>
2025-01-26 13:48:51 +0300

    fix: :bug: fixdeploy

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,23 +1,10 @@
-import { CategoriesPage } from "@/modules/Categories/pages/CategoriesPage"
-import { EmployeePage } from "@/modules/Employee"
-import { PageNotFound } from "@/modules/NotFound/pages/PageNotFound"
-import { RoomsPage } from "@/modules/Rooms/pages/RoomsPage"
-import { AuthorizationPage } from "@/modules/Authorization/pages/AuthorizationPage"
-import { ClientsPage } from "@/modules/Clients/pages/ClientsPage"
 import { Layout } from "@/shared/ui/Layouts/Layout"
 import { RouteObject } from "react-router-dom"
-import { BookingPage } from "@/modules/Booking/pages/BookingPage/BookingPage"
-import { CreateBookingPage } from "@/modules/Booking/pages/CreateBookingPage/CreateBookingPage"
-import { ClientPage } from "@/modules/Clients/pages/ClientPage.tsx"
 import { BookingGridPage } from "@/modules/Booking/pages/BookingGridPage/BookingGridPage"
 import { LayoutWithFooter } from "@/shared/ui/Layouts/LayoutWithFooter"
 import { LayoutBookingGrid } from "@/shared/ui/Layouts/LayoutBookingGrid"
 import { lazy, Suspense } from "react"
-import { RouteObject } from "react-router-dom"
-import { Layout } from "@/shared/ui/Layouts/Layout"
-import { LayoutWithFooter } from "@/shared/ui/Layouts/LayoutWithFooter"
 import { CreatePetPage, PetPage } from "@/modules/Pets"
-
 
 const CategoriesPage = lazy(() =>
   import("@/modules/Categories/pages/CategoriesPage").then(module => ({
